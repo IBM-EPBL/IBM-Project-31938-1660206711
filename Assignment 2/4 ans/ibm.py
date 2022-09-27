2b1bddff8560cba71b895c10c04c2e7bd5ea3b36
16Gokul <113768332+16Gokul@users.noreply.github.com>
2022-09-27 23:49:54 +0530

    Add files via upload

---
+++ Assignment 2/4 ans/ibm.py
@@ -0,0 +1,28 @@
+import ibm_db
+
+hostname = '19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud'
+uid = 'lcx41082'
+pwd = 'TyLII4Gaq6d1dI9x'
+driver = "{IBM DB2 ODBC DRIVER}"
+db = 'bludb'
+port = '30699'
+protocol = 'TCPIP'
+cert = 'Certificate.crt'
+
+dsn = (
+    "DATABASE = {0};"
+    "HOSTNAME = {1};"
+    "PORT = {2};"
+    "UID = {3};"
+    "SECURITY = SSL;"
+    "SSlServerCertificate = {4};"
+    "PWD = {5};"
+).format(db, hostname, port, uid, cert, pwd)
+print(dsn)
+try:
+    db2 = ibm_db.connect(dsn, " ", " ")
+    print("connect to database")
+except:
+    print("unable to connect ", ibm_db.conn_errormsg())
+
+
